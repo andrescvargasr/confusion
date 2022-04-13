5c3d27b69e46b3a9003a3a113bd39c4b4c481a39
andrescvargasr <andresc.vargasr@gmail.com>
2022-04-12 20:56:00 -0500

    feat: Header and Footer

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+          <Navbar dark>
+          <div className='container'>
+            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Jumbotron>
+            <div className='container'>
+                <div className='row row-header'>
+                    <div className='col-12 col-sm-6'>
+                        <h1>Ristorante Con Fusion</h1>
+                        <p>We take inspiration from the Woeld's best cuisines, and create a uniquye fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                    </div>
+                </div>
+            </div>
+        </Jumbotron>
+      </React.Fragment>
+    )
+  }
+}
